6827dcfbaaa9e379619236e55f70cf3a218a8055
unknown <ilove.programming420@gmail.com>
2015-08-30 09:24:57 +0530

    Initial commit

---
+++ java/br2sinfotech/dailyplanner/LauncherActivity.java
@@ -0,0 +1,14 @@
+package br2sinfotech.dailyplanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class LauncherActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+
+}
